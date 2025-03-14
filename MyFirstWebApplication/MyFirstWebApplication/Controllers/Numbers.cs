d0662bc6132462880e0e9b70f2ac99500c64374e
Gabriel Mayer <gabriel.mayer@student.htldornbirn.at>
2025-03-14 13:59:34 +0100

    done with issues

---
+++ MyFirstWebApplication/MyFirstWebApplication/Controllers/Numbers.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstWebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Numbers : ControllerBase
+    {
+        [HttpPost("increment")]
+        public ActionResult<int> Increment([FromBody] int number)
+        {
+            return Ok(number + 1);
+        }
+
+        [HttpPut("Sum")]
+        public ActionResult<int> Increment1([FromBody] Sum sum)
+        {
+            return Ok(sum.ZahlA + sum.ZahlB);
+        }
+    }
+
+
+}
